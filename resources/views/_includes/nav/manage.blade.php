8a7591e292dda88207d547ee41548cf70b4395aa
mattierocks <mattierocks@gmail.com>
2018-01-06 14:24:23 -0500

    add laraveleasynav to highlight active classes

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -4,16 +4,16 @@
             General
         </p>
         <ul class="menu-list">
-            <li><a href="{{route('manage.dashboard')}}">Dashboard</a></li>
+            <li><a href="{{route('manage.dashboard')}}" class="{{ Nav::isRoute('manage.dashboard') }}">Dashboard</a></li>
         </ul>
 
         <p class="menu-label">
             Administration
         </p>
         <ul class="menu-list">
-            <li><a href="{{ route('users.index') }}">Manage Users</a></li>
+            <li><a href="{{ route('users.index') }}" class="{{ Nav::isResource('users') }}">Manage Users</a></li>
             <li>
-                <a class="has-submenu">Roles &amp; Permissions</a>
+                <a class="has-submenu {{ Nav::hasSegment(['roles', 'permissions'], 2) }}">Roles &amp; Permissions</a>
                 <ul class="submenu">
                     <li><a href="{{ route('roles.index') }}">Roles</a></li>
                     <li><a href="{{ route('permissions.index') }}">Permissions</a></li>
